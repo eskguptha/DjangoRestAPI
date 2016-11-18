b71969bf58ef6278e568f6e3fe0cb9d7286729e1
eskguptha <santhosh.e@revenuemantra.com>
2016-11-18 23:04:59 +0530

    restapi chages

---
+++ djangorestapi/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test1',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
